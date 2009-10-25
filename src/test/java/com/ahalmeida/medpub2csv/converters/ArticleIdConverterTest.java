4639ed85e1bde859daf591b188c84a53e18169d9
Adriano Henrique de Almeida <adrianoalmeida7@gmail.com>
2009-10-24 22:34:24 -0200

    a little more tests

--- src/test/java/com/ahalmeida/medpub2csv/converters/ArticleIdConverterTest.java
+++ src/test/java/com/ahalmeida/medpub2csv/converters/ArticleIdConverterTest.java
@@ -4,9 +4,12 @@ import org.junit.Test;
 
 import com.ahalmeida.medpub2csv.model.ArticleId;
 import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 
@@ -30,5 +33,33 @@ public class ArticleIdConverterTest {
 		ArticleId id = (ArticleId)new ArticleIdConverter().unmarshal(reader, null);
 
 		assertEquals("foo", id.getValue());
+	}
+
+	@Test
+	public void articleIdIsConversible() {
+		boolean isConversible = new ArticleIdConverter().canConvert(ArticleId.class);
+		assertTrue(isConversible);
+	}
+
+	@Test
+	public void marshallingArticleIdSetsTheIdTypeAttribute() {
+		ArticleId id = new ArticleId();
+		id.setIdType("foo");
+
+		HierarchicalStreamWriter writer = mock(HierarchicalStreamWriter.class);
+		new ArticleIdConverter().marshal(id, writer, null);
+
+		verify(writer).addAttribute("IdType", "foo");
+	}
+
+	@Test
+	public void marshallingArticleIdSetsTheValue() {
+		ArticleId id = new ArticleId();
+		id.setValue("foo");
+
+		HierarchicalStreamWriter writer = mock(HierarchicalStreamWriter.class);
+		new ArticleIdConverter().marshal(id, writer, null);
+
+		verify(writer).setValue("foo");
 	}
 }
